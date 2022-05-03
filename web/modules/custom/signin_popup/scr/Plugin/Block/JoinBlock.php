e0f4f3b462037398db1600f449587eed6b13fc82
Inna <isoyan.inn@gmail.com>
2022-05-03 15:29:13 +0400

    create custom module for signin popup

---
+++ web/modules/custom/signin_popup/scr/Plugin/Block/JoinBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\signin_popup\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Join Now' Block.
+ *
+ * @Block(
+ *   id = "join_now_block",
+ *   admin_label = @Translation("Join now"),
+ * )
+ */
+class JoinBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#theme' => 'join_now_block',
+    ];
+  }
+
+}
