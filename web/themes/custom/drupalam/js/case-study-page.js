4b29ecd9cddc1147d4f623129a72f5f0ede94b63
Ivan Abramenko <ivan@829llc.com>
2022-06-01 10:17:43 +0400

    86 - Add Slick slider

--- web/themes/custom/drupalam/js/case-study-page.js
+++ web/themes/custom/drupalam/js/case-study-page.js
@@ -1,27 +1,22 @@
 (function ($, Drupal, drupalSettings) {
-
   'use strict';
 
-  // Simple Image Slider
-  var $images = [];
+  Drupal.behaviors.boostrapTheme = {
+    attach: function (context, settings) {
+      $('.node--type-case-study .field--name-field-screens-media').slick();
+    }
+  };
 
-  $('.sis-images .field--items img').each(function (index, el) {
-    var $el = $(el);
-    $images.push({img : $el.attr('src')});
-  });
-
-  $(".sis-wrapper").simpleImageSlider({
-    'images': $images
-  });
-
-  // Related projects carousel
-  var items = $('.related-projects-block .views-element-container .item');
+  $(document).ready(function() {
+    // Related projects carousel
+    var items = $('.related-projects-block .views-element-container .item');
     for (var i = 0; i < items.length; i += 2) {
       items.slice(i, i + 2).wrapAll("<div class='page-wrapper'></div>");
     }
-  $('.related-projects-block .views-element-container').pexetoCarousel();
+    $('.related-projects-block .views-element-container').pexetoCarousel();
 
-  // Set the same heigth to carousel items by 'MatchHeight'
-  $('.related-projects-block .views-element-container .item .node').matchHeight();
+    // Set the same heigth to carousel items by 'MatchHeight'
+    $('.related-projects-block .views-element-container .item .node').matchHeight();
+  });
 
 })(jQuery, Drupal, drupalSettings);
